97d5eec0ae8d42af6b8a6ac01d4f6d1cc00d27fd
yassir-jeraidi <yassir.junior.03@gmail.com>
2025-11-10 19:05:27 +0100

    feat: add new certifs

--- src/features/profile/data/certifications.ts
+++ src/features/profile/data/certifications.ts
@@ -2,55 +2,119 @@ import type { Certification } from "../types/certifications";
 
 export const CERTIFICATIONS: Certification[] = [
   {
+    title: "Software Engineer",
+    issuer: "HackerRank",
+    issuerIconName: "hackerrank",
+    issueDate: "2025-11-05",
+    credentialID: "c5a32a9df6c0",
+    credentialURL:
+      "https://www.hackerrank.com/certificates/c5a32a9df6c0",
+  },
+  {
+    title: "NDG Linux Unhatched",
+    issuer: "OpenEDG (cisco)",
+    issuerIconName: "openedg (cisco)",
+    issueDate: "2025-03-07",
+    credentialID: "NDG-Linux-Unhatched-Yassir-Jeraidi-2025",
+    credentialURL:
+      "/Linux_Unhatched_certificate_yassir-jeraidi-etu-etu-univh2c-ma_cd40b802-bde1-497f-b15b-0580a709e0f2.pdf",
+  },
+  {
     title: "AWS Educate: Introduction to Cloud 101",
     issuer: "AWS Educate",
     issuerIconName: "aws",
-    issueDate: "2025-10-28",
+    issueDate: "2024-11-04",
     credentialID: "",
     credentialURL:
       "https://www.credly.com/badges/744c578c-2030-4c10-9046-537fafa418e2/linked_in_profile",
   },
   {
-    title: "Problem Solving",
-    issuer: "HackerRank",
-    issuerIconName: "hackerrank",
-    issueDate: "2025-10-28",
-    credentialID: "",
-    credentialURL:
-      "https://www.hackerrank.com/certificates/iframe/765d14103f23",
-  },
-  {
     title: "Introduction to Python",
     issuer: "365 Data Science",
     issuerIconName: "365datascience",
-    issueDate: "2025-10-28",
+    issueDate: "2024-11-04",
     credentialID: "CC-B206BC2865",
     credentialURL:
       "https://learn.365datascience.com/certificates/CC-B206BC2865/",
   },
   {
+    title: "Problem Solving",
+    issuer: "HackerRank",
+    issuerIconName: "hackerrank",
+    issueDate: "2022-11-12",
+    credentialID: "",
+    credentialURL:
+      "https://www.hackerrank.com/certificates/iframe/765d14103f23",
+  },
+  {
     title: "Data Types in Node.js",
     issuer: "Udemy",
     issuerIconName: "udemy",
-    issueDate: "2025-10-28",
+    issueDate: "2023-07-21",
     credentialID: "UC-ed548d3e-18a2-4707-b1bc-ec5b268dc3b3",
     credentialURL:
       "https://www.udemy.com/certificate/UC-ed548d3e-18a2-4707-b1bc-ec5b268dc3b3/",
   },
   {
     title: "JavaScript Essentiel 1",
-    issuer: "OpenEDG",
-    issuerIconName: "openedg",
-    issueDate: "2025-10-28",
-    credentialID: "",
-    // credentialURL: "#",
+    issuer: "OpenEDG (cisco)",
+    issuerIconName: "openedg (cisco)",
+    // 21 october 2022
+    issueDate: "2022-10-21",
+    credentialID: "01ea8b89-523c-4c46-b55b-dd9dbfc307ca",
+    credentialURL: "/Partner-_JavaScript_Essentials_1_-JSE-_certificate_yassir-jeraidi-ofppt-edu-ma_01ea8b89-523c-4c46-b55b-dd9dbfc307ca.pdf",
   },
   {
     title: "Programming Essentiel in Python",
-    issuer: "OpenEDG",
-    issuerIconName: "openedg",
-    issueDate: "2025-10-28",
-    credentialID: "",
-    // credentialURL: "#",
+    issuer: "OpenEDG (cisco)",
+    issuerIconName: "openedg (cisco)",
+    issueDate: "2022-10-23",
+    credentialID: "83adae4f-1cd7-41c1-b127-5a03d9f5e9d5",
+    credentialURL: "/Partner-_PCAP_-_Programming_Essentials_in_Python_certificate_yassir-jeraidi-ofppt-edu-ma_83adae4f-1cd7-41c1-b127-5a03d9f5e9d5.pdf",
+  },
+  {
+    title : "HTML5",
+    issuer : "Solo Learn",
+    issuerIconName : "sololearn",
+    issueDate : "2022-03-26",
+    credentialID : "CT-IK2HWK4D",
+    credentialURL :
+      "https://www.sololearn.com/certificates/CT-IK2HWK4D",
+  },
+  {
+    title : "CSS3",
+    issuer : "Solo Learn",
+    issuerIconName : "sololearn",
+    issueDate : "2022-09-27",
+    credentialID : "CT-X8ZNLGJ4",
+    credentialURL :
+      "https://www.sololearn.com/en/certificates/CT-X8ZNLGJ4",
+  },
+  {
+    title : "Techniques d'Analyse et Visualisation des données",
+    issuer : "CITT & Afri connect +",
+    issuerIconName : "citt",
+    issueDate : "2024-02-21",
+    credentialID : "N/A",
+    credentialURL :
+      "/certificat de formation IOT F6 JERAIDI YASSIR.pdf",
+  },
+   {
+    title : "La Sécurité de la communication numérique des IoT intra et inter net",
+    issuer : "CITT & Afri connect +",
+    issuerIconName : "citt",
+    issueDate : "2024-02-21",
+    credentialID : "N/A",
+    credentialURL :
+      "/certificat de formation IOT F5 JERAIDI YASSIR.pdf",
+  },
+  {
+    title : "Initiation à la programmation embarquée avec STM32 Cube IDE",
+    issuer : "CITT & Afri connect +",
+    issuerIconName : "citt",
+    issueDate : "2024-02-23",
+    credentialID : "N/A",
+    credentialURL :
+      "/certificat de formation IOT F7 F8 JERAIDI YASSIR.pdf",
   },
 ];
