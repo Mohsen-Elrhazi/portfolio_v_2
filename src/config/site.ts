1c1c80d3c76b289a5efee10703605ed4e458f9c0
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2025-12-24 09:35:07 +0100

    fix site.tsx

--- src/config/site.ts
+++ src/config/site.ts
@@ -32,11 +32,11 @@ export const MAIN_NAV: NavItem[] = [
 ];
 
 export const GITHUB_USERNAME = "Mohsen-Elrhazi";
-// export const SOURCE_CODE_GITHUB_REPO = "ncdai/chanhdai.com";
-// export const SOURCE_CODE_GITHUB_URL = "https://github.com/ncdai/chanhdai.com";
+export const SOURCE_CODE_GITHUB_REPO = "ncdai/chanhdai.com";
+export const SOURCE_CODE_GITHUB_URL = "https://github.com/ncdai/chanhdai.com";
 
 export const UTM_PARAMS = {
-  // utm_source: "chanhdai.com",
-  // utm_medium: "portfolio_website",
-  // utm_campaign: "referral",
+  utm_source: "chanhdai.com",
+  utm_medium: "portfolio_website",
+  utm_campaign: "referral",
 };
