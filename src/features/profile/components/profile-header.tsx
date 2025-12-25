768a5917366f82b68d09d4b73f57d0116251bd02
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2025-12-25 12:21:05 +0100

    ajuster size de nom en version mobile

--- src/features/profile/components/profile-header.tsx
+++ src/features/profile/components/profile-header.tsx
@@ -49,7 +49,7 @@ export function ProfileHeader() {
      
 
         <div className="border-t border-edge">
-          <h1 className="flex items-center pl-4 text-2xl sm:text-3xl font-semibold">
+          <h1 className="flex items-center pl-4 text-3xl font-semibold">
             {USER.displayName}
             &nbsp;
             {/* <SimpleTooltip content="Vérifié"> */}
