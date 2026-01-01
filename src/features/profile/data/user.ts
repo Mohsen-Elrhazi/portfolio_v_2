5d860977a5eedf62f7ef3d4c70e2d73969b8ceca
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2026-01-01 19:01:02 +0100

    update cv

--- src/features/profile/data/user.ts
+++ src/features/profile/data/user.ts
@@ -33,7 +33,6 @@ Développeur Full Stack, passionné par la conception et la création de solutio
 
   // avatar: "mohsen.jpeg",
   avatar: "mohsen2_ai.png",
-  // ogImage: '' , // "https://assets.chanhdai.com/images/screenshot-og-image-light.png?t=1759581475"
   ogImage: "https://mohsen-elrhazi.tech/mohsen2_ai.png",
   namePronunciationUrl: "/audio/audio.mp3",
   keywords: ["Elrhazi Mohsen", "elrhazi mohsen", "mohsen elrhazi", ""],
